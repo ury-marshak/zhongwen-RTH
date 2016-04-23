1b8342d15436cd935fe1c8b1cb382b18a8809375
Ury Marshak <ury.marshak@gmail.com>
2016-04-24 02:52:04 +0300

    update to newer RTH data

--- rth-cedict/index-cedict.lisp
+++ rth-cedict/index-cedict.lisp
@@ -112,51 +112,142 @@
   (make-dict fname-in fname-out +rth2fields+ "RTH2"))
 
 
+(defparameter +rth-new-fields+
+  '(:trad :keyword :keyword-info :primitive :reading
+    :rth-index :lesson :strokes :story :hint :comments :simp
+    ))
+
+(defun rth-new-dict (fname-in fname-out)
+  (make-dict fname-in fname-out +rth-new-fields+ "RTH"))
+
+
+(defun replace-all (string part replacement &key (test #'char=))
+"Returns a new string in which all the occurences of the part 
+is replaced with replacement."
+    (with-output-to-string (out)
+      (loop with part-length = (length part)
+            for old-pos = 0 then (+ pos part-length)
+            for pos = (search part string
+                              :start2 old-pos
+                              :test test)
+            do (write-string string out
+                             :start old-pos
+                             :end (or pos (length string)))
+            when pos do (write-string replacement out)
+              while pos)))
+
+
 (defun make-pleco ()
-  (let ((data1 (load-data "~/work/index-cedict/RTH1notes.txt" +rth1fields+))
-        (data2 (load-data "~/work/index-cedict/RTH2notes.txt" +rth2fields+))
-        (fname-out "~/work/index-cedict/plecoRTH.txt"))
+  (let ((data1 (load-data "~/work/zhongwen-RTH/rth-cedict/RTH1notes.txt" +rth1fields+))
+        (data2 (load-data "~/work/zhongwen-RTH/rth-cedict/RTH2notes.txt" +rth2fields+))
+        (fname-out "~/work/zhongwen-RTH/rth-cedict/plecoRTH.txt"))
     
     (with-open-file (f-out fname-out
                            :external-format :utf-8
                            :direction :output
                            :if-does-not-exist :create
                            :if-exists :supersede)
-      (flet ((write-data (data dictname)
-               (loop for rec in data
-                     do (destructuring-bind  (&key rth-index trad simp keyword
-                                                   primitive story &allow-other-keys)
-                            rec
+      (labels ((replace-html (s)
+                 (replace-all s "&nbsp;" " "))
+               (write-data (data dictname)
+                 (loop for rec in data
+                       do (destructuring-bind  (&key rth-index trad simp keyword
+                                                     primitive story &allow-other-keys)
+                              rec
 
-                          (setf trad (trim-string trad))
-                          (setf simp (trim-string simp))
+                            (setf trad (trim-string trad))
+                            (setf simp (trim-string simp))
                           
-                          (when (string= simp trad)
-                            (setf simp ""))
-                          (unless (zerop (length simp))
-                            (setf simp (format nil "[~A]" simp)))
+                            (when (string= simp trad)
+                              (setf simp ""))
+                            ;; (unless (zerop (length simp))
+                            ;;   (setf simp (format nil "[~A]" simp)))
                           
-                          (unless primitive
-                            (setf primitive ""))
-                          (unless story
-                            (setf story ""))
+                            (unless primitive
+                              (setf primitive ""))
+                            (unless story
+                              (setf story ""))
 
+                            (setf trad (replace-html trad))
+                            (setf simp (replace-html simp))
+                            (setf keyword (replace-html keyword))
                           
-                          (format f-out
-                                  "~A~A~C~C~C~A~C  ~A (~A)~%"
-                                  trad simp #\Tab
-                                  #\Tab ;; no pinyin
-                                  #\U+EAB2 keyword #\U+EAB3
-                                  rth-index dictname
+                            (let ((headword trad))
+                              (when (plusp (length simp))
+                                (setf headword
+                                      (format nil "~A[~A]" simp trad) ))
+                              (format f-out
+                                      "~A~C~C~C~A~C  ~A (~A)~%"
+                                      headword #\Tab
+                                      #\Tab ;; no pinyin
+                                      #.(code-char #xEAB2) keyword #.(code-char #xEAB3)
+                                      rth-index dictname
                                   
                                   
-                                  ;; (if (and *compile-stories* (plusp (length primitive)))
-                                  ;;     (format nil "/(Prim: ~A)" primitive)
-                                  ;;     "")
-                                  ;; (if (and *compile-stories* (plusp (length story)))
-                                  ;;     (format nil "/~A" story)
-                                  ;;     "")
-                                  )))))
+                                      ;; (if (and *compile-stories* (plusp (length primitive)))
+                                      ;;     (format nil "/(Prim: ~A)" primitive)
+                                      ;;     "")
+                                      ;; (if (and *compile-stories* (plusp (length story)))
+                                      ;;     (format nil "/~A" story)
+                                      ;;     "")
+                                      ))))))
+        (write-data data1 "RTH1")
+        (write-data data2 "RTH2"))
+      
+      ;;(format f-out "~%")
+      )))
+
+
+(defun make-tsv ()
+  (let ((data1 (load-data "~/work/zhongwen-RTH/rth-cedict/RTH1notes.txt" +rth1fields+))
+        (data2 (load-data "~/work/zhongwen-RTH/rth-cedict/RTH2notes.txt" +rth2fields+))
+        (fname-out "~/work/zhongwen-RTH/rth-cedict/RTH.tsv"))
+    
+    (with-open-file (f-out fname-out
+                           :external-format :utf-8
+                           :direction :output
+                           :if-does-not-exist :create
+                           :if-exists :supersede)
+      (labels ((replace-html (s)
+                 (replace-all s "&nbsp;" " "))
+               (write-data (data dictname)
+                 (loop for rec in data
+                       do (destructuring-bind  (&key rth-index trad simp keyword
+                                                     primitive story &allow-other-keys)
+                              rec
+
+                            (setf trad (trim-string trad))
+                            (setf simp (trim-string simp))
+                          
+                            (when (string= simp trad)
+                              (setf simp ""))
+                            ;; (unless (zerop (length simp))
+                            ;;   (setf simp (format nil "[~A]" simp)))
+                          
+                            (unless primitive
+                              (setf primitive ""))
+                            (unless story
+                              (setf story ""))
+
+                          
+                            (setf trad (replace-html trad))
+                            (setf simp (replace-html simp))
+                            (setf keyword (replace-html keyword))
+                          
+                            (format f-out
+                                    "~A~C~A~C~A~C~A~C~A~C~%"
+                                    trad #\Tab simp #\Tab
+                                    keyword #\Tab
+                                    rth-index #\Tab dictname #\Tab
+                                  
+                                  
+                                    ;; (if (and *compile-stories* (plusp (length primitive)))
+                                    ;;     (format nil "/(Prim: ~A)" primitive)
+                                    ;;     "")
+                                    ;; (if (and *compile-stories* (plusp (length story)))
+                                    ;;     (format nil "/~A" story)
+                                    ;;     "")
+                                    )))))
         (write-data data1 "RTH1")
         (write-data data2 "RTH2"))
       
@@ -167,14 +258,20 @@
 
 
 #|
- (rth1-dict "~/work/index-cedict/RTH1notes.txt"
-         "~/work/index-cedict/rth1dict.u8")
+ (rth1-dict "~/work/zhongwen-RTH/rth-cedict/RTH1notes.txt"
+         "~/work/zhongwen-RTH/rth-cedict/rth1dict.u8")
 
- (rth2-dict "~/work/index-cedict/RTH2notes.txt"
-         "~/work/index-cedict/rth2dict.u8")
+ (rth2-dict "~/work/zhongwen-RTH/rth-cedict/RTH2notes.txt"
+         "~/work/zhongwen-RTH/rth-cedict/rth2dict.u8")
 
 |#
 
 #|
- (cl-user::load-asd "~/work/index-cedict/index-cedict.asd")
+ (cl-user::load-asd "~/work/zhongwen-RTH/rth-cedict/index-cedict.asd")
+|#
+#|
+ (rth-new-dict "~/work/zhongwen-RTH/rth-cedict/RTHplusGrades1-8.tsv"
+         "~/work/zhongwen-RTH/rth-cedict/rthdict.u8")
+
+
 |#
